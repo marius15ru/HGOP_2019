6f6dfafc1615ce7237451a3ce043fb9f639863a8
marius15ru <marius15@ru.is>
2019-12-07 10:51:53 +0000

    Update jenkins_deploy.sh

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -23,7 +23,7 @@ terraform apply -auto-approve
 echo "Game API running at " + $(terraform output public_ip)
 
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./script/docker_compose_up.sh $GIT_COMMIT"
 
 set -o errexit
 

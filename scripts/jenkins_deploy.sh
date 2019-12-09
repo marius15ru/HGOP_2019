5e24a09f2d220277e53d0a527f844f75b4fcd310
marius15ru <marius15@ru.is>
2019-12-09 14:08:53 +0000

    Update jenkins_deploy.sh

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -5,12 +5,12 @@ GIT_COMMIT=$1
 # We need to move some files around, because of the terraform state limitations.
 mkdir -p /var/lib/jenkins/terraform/hgop/production
 mkdir -p /var/lib/jenkins/terraform/hgop/production/scripts
-rm -f /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
-cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
+# rm -f /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
+# cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
 rm -f /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh
-cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh
-rm -f /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
-cp docker-compose.yml /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
+cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh  && echo -e "Cp Sucess" || echo -e "Cp Fail"
+# rm -f /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
+# cp docker-compose.yml /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
 : '
 rm -f /var/lib/jenkins/terraform/hgop/production/*.tf
 cp *.tf /var/lib/jenkins/terraform/hgop/production

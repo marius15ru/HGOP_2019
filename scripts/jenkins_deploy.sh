5b37e6ed1497ced4495f06bc72dfa3a1556ce3d6
marius15ru <marius15@ru.is>
2019-12-09 14:54:09 +0000

    Update jenkins_deploy.sh

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -5,13 +5,14 @@ GIT_COMMIT=$1
 # We need to move some files around, because of the terraform state limitations.
 mkdir -p /var/lib/jenkins/terraform/hgop/production
 mkdir -p /var/lib/jenkins/terraform/hgop/production/scripts
-# rm -f /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
-# cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
+rm -f /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
+cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/production/scripts/initialize_game_api_instance.sh
+ls $echo(/var/lib/jenkins/terraform/hgop/production/)
 rm -f /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh
 cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/production/scripts/docker_compose_up.sh  && echo -e "Cp Sucess" || echo -e "Cp Fail"
-# rm -f /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
-# cp docker-compose.yml /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
-: '
+rm -f /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
+cp docker-compose.yml /var/lib/jenkins/terraform/hgop/production/docker-compose.yml
+
 rm -f /var/lib/jenkins/terraform/hgop/production/*.tf
 cp *.tf /var/lib/jenkins/terraform/hgop/production
 
@@ -24,7 +25,7 @@ echo "Game API running at " + $(terraform output public_ip)
 
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
-'
+
 set -o errexit
 
 exit 0

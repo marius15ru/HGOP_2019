b2d9462aca2947b59ade770da43ff71733b9e608
marius15ru <marius15@ru.is>
2019-12-12 15:59:51 +0000

    Api and capacity test

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -5,14 +5,11 @@ set -euxo pipefail
 GIT_COMMIT=$1 TEST=$2
 TEST_ENV="${TEST}test"
 
-sh "./jenkins_deploy.sh ${GIT_COMMIT} ${TEST}"
+sh "./jenkins_deploy.sh ${GIT_COMMIT} ${TEST_ENV}"
 
 cd /var/lib/jenkins/workspace/Github_Pipeline_HGOP2019/game_api
-API_URL=$(terraform output public_dns):3000 npm run test:$TEST_KIND
-cd /var/lib/jenkins/terraform/hgop/$ENV
-terraform destroy -auto-approve -var environment=$ENV || exit 1
-
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
+API_URL=$(terraform output public_dns):3000 npm run test:$TEST
+cd /var/lib/jenkins/terraform/hgop/$TEST_ENV
+terraform destroy -auto-approve -var environment=$TEST_ENV || exit 1
 
 exit 0

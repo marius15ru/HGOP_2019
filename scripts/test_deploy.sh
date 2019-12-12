5fe87331ecdc9969b001e4be197bc56129accc8f
marius15ru <marius15@ru.is>
2019-12-12 16:04:50 +0000

    Api and capacity test

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 GIT_COMMIT=$1 TEST=$2
 TEST_ENV="${TEST}test"
 
-sh "./jenkins_deploy.sh ${GIT_COMMIT} ${TEST_ENV}"
+sh "./scripts/jenkins_deploy.sh ${GIT_COMMIT} ${TEST_ENV}"
 
 cd /var/lib/jenkins/workspace/Github_Pipeline_HGOP2019/game_api
 API_URL=$(terraform output public_dns):3000 npm run test:$TEST

35884ab32b58091cacbc7d865c72ede055204fd1
marius15ru <marius15@ru.is>
2019-12-16 00:39:33 +0000

    game_client added build and push scripts

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -5,5 +5,6 @@ set -euxo pipefail
 GIT_COMMIT=$1
 
 docker build -t marius15/game_api:$GIT_COMMIT game_api/
+docker build -t marius15/game_client:$GIT_COMMIT game_client/
 
 exit 0
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -5,5 +5,6 @@ set -euxo pipefail
 GIT_COMMIT=$1
 
 docker push marius15/game_api:$GIT_COMMIT
+docker push marius15/game_client:$GIT_COMMIT
 
 exit 0

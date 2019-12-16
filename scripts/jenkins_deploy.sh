bcf26f07c60898ef39172f16bfedded3eef0f9b3
marius15ru <marius15@ru.is>
2019-12-16 00:09:03 +0000

    Update jenkins_deploy.sh

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -29,6 +29,7 @@ terraform destroy -auto-approve -var environment=$ENV || exit 1
 terraform apply -auto-approve -var environment=$ENV || exit 1
 
 echo "Game API running at " + $(terraform output public_ip)
+API_URL="http://$(terraform output public_ip):3000"
 
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
